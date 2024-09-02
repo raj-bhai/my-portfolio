5565087336e5651bd8f2c0c9b0a39802427f4eb7
Rajkiran Kalowar <rajkiranjnv1@gmail.com>
2024-09-03 03:41:47 +0530

    fix

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,7 +2,7 @@ export default function Contact() {
     return (
       <section className="text-center py-20">
         <h1 className="text-4xl font-bold mb-4">Contact Me</h1>
-        <p className="text-lg">If you'd like to work together, feel free to reach out!</p>
+        <p className="text-lg">If you&apos;d like to work together, feel free to reach out!</p>
       </section>
     )
   }
