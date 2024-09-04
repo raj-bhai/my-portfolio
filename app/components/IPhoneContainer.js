bc5bf28cc6d558a993e13371d4f1caeaed5d6315
Rajkiran Kalowar <rajkiranjnv1@gmail.com>
2024-09-05 04:16:23 +0530

    gg

--- app/components/IPhoneContainer.js
+++ app/components/IPhoneContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IPhoneContainer = ({ children }) => {
   return (
-    <div className="bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 dark:from-indigo-700 dark:via-purple-700 dark:to-pink-700 rounded-lg shadow-md p-6 w-[280px] h-[498px] border-8 border-gray-300 rounded-[40px] relative overflow-hidden">
+    <div className="bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 dark:from-indigo-700 dark:via-purple-700 dark:to-pink-700 rounded-[30px] shadow-md p-6 w-[280px] h-[498px] border-8 border-gray-300 rounded-[40px] relative overflow-hidden">
       {/* Speaker Section */}
       <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-20 h-4 bg-black rounded-b-lg"></div>
 
