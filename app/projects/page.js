0d09333ba1d16523274c8fdf89a20de94df41d2c
Rajkiran Kalowar <rajkiranjnv1@gmail.com>
2024-12-24 02:00:15 +0530

    project animation

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,18 +1,43 @@
-
+"use client"
 import ProjectCard from "@/components/ProjectCard";
 import ProjectsData from "@/data/projectsData";
+import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.2,
+      duration: 0.6,
+      when: "beforeChildren",
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
 
 export default function Projects() {
   return (
     <section className="min-h-screen text-center pb-20">
       <h1 className="text-4xl font-bold mb-4">Projects</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <motion.div
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
         {ProjectsData.map((project) => (
-          <ProjectCard key={project.id} project={project} />
+          <motion.div key={project.id} variants={itemVariants}>
+            <ProjectCard project={project} />
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </section>
-
   );
 }
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,39 +1,46 @@
 import Link from "next/link";
 import TechnologyIcons from "./TechnologyIcons";
+import { motion } from "framer-motion";
 
+const cardVariants = {
+  hover: {
+    scale: 1.05,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="relative bg-white dark:bg-gray-800 text-gray-800 dark:text-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition-all">
-      {/* Project Title */}
+    <motion.div
+      className="relative bg-white dark:bg-gray-800 text-gray-800 dark:text-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition-all"
+      whileHover="hover"
+      variants={cardVariants}
+    >
       <h2 className="text-2xl font-semibold mb-4 hover:text-yellow-500">{project.title}</h2>
-
-      {/* Project Introduction */}
       <p className="text-gray-600 dark:text-gray-300 text-ellipsis overflow-hidden mb-4"
-                style={{
-                  WebkitLineClamp: 2,
-                  display: "-webkit-box",
-                  WebkitBoxOrient: "vertical",
-                }}
-      >{project.introduction}</p>
-
-      {/* Project Description (Truncated to two lines) */}
+         style={{
+           WebkitLineClamp: 2,
+           display: "-webkit-box",
+           WebkitBoxOrient: "vertical",
+         }}
+      >
+        {project.introduction}
+      </p>
       <div className="mb-4">
         <strong className="text-lg">Description:</strong>
-        <p
-          className="text-gray-500 dark:text-gray-400 text-ellipsis overflow-hidden"
-          style={{
-            WebkitLineClamp: 2,
-            display: "-webkit-box",
-            WebkitBoxOrient: "vertical",
-          }}
+        <p className="text-gray-500 dark:text-gray-400 text-ellipsis overflow-hidden"
+           style={{
+             WebkitLineClamp: 2,
+             display: "-webkit-box",
+             WebkitBoxOrient: "vertical",
+           }}
         >
           {project.description}
         </p>
       </div>
-
-      {/* Technologies Used */}
-      <div className="mb-4  ">
+      <div className="mb-4">
         <strong className="text-lg">Technologies Used:</strong>
         <ul className="list-none flex flex-wrap justify-center gap-2">
           {project.technologies.map((tech, index) => {
@@ -47,14 +54,10 @@ const ProjectCard = ({ project }) => {
           })}
         </ul>
       </div>
-
-      {/* Project Role */}
       <div className="mb-4">
         <strong className="text-lg">My Role:</strong>
         <p className="text-gray-500 dark:text-gray-400">{project.role}</p>
       </div>
-
-      {/* View Details Button */}
       <div className="mt-4 mb-4">
         <Link href={`/projects/${project.id}`} passHref>
           <button className="px-6 py-2 text-white bg-yellow-500 rounded-full hover:bg-yellow-600 transition-colors">
@@ -62,12 +65,10 @@ const ProjectCard = ({ project }) => {
           </button>
         </Link>
       </div>
-
-      {/* Bottom decorative border */}
       <div className="absolute bottom-4 left-4 right-4 flex justify-between items-center">
         <div className="absolute bottom-0 left-0 right-0 h-1 bg-yellow-500 transition-all duration-300" />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
