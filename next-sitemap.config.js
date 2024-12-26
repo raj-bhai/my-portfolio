0aad9cfec509e5a6c49072a31a33fc3a18b0be2f
Rajkiran Kalowar <rajkiranjnv1@gmail.com>
2024-12-26 21:07:34 +0530

    site-map

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.iamrajklwr.com', // Your website URL
+    generateRobotsTxt: true, // (optional) Generate robots.txt file
+    exclude: [], // Exclude pages if necessary
+  };
+  
